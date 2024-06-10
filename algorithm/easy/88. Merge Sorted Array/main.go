327e265831603bc45be050b968bae5920a177a80
reynoldputra <reynoldputra1@gmail.com>
2024-06-10 19:30:15 +0700

    fix: update 88. merge sorted array

--- algorithm/easy/88. Merge Sorted Array/main.go
+++ algorithm/easy/88. Merge Sorted Array/main.go
@@ -1,6 +1,21 @@
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    var arr []int
-    arr = append(nums1[:m], nums2[:n]...)
-    sort.Ints(arr)
-    fmt.Println(arr)
+    idx1 := m - 1
+    idx2 := n - 1
+    i := len(nums1) - 1
+
+    if m == 0 {
+        copy(nums1, nums2)
+        return
+    }
+
+    for i >= 0 {
+        if idx2 < 0 || (idx1 >= 0 && nums1[idx1] > nums2[idx2]) {
+            nums1[i] = nums1[idx1]
+            idx1--
+        } else {
+            nums1[i] = nums2[idx2]
+            idx2--
+        }
+        i--
+    }
 }
